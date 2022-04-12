8a76299c70b4874a9dac203b4440addb1a58eb61
kalysbek-z <kalysbek_z@auca.kg>
2022-04-12 21:13:05 +0600

    finished v1

--- bot.go
+++ bot.go
@@ -22,52 +22,53 @@ func TelegramBotReplier(service *NotifangaService) {
 
 	// wh, _ := tgbotapi.NewWebhookWithCert()
 
-	for {
-		update := tgbotapi.NewUpdate(0)
-		update.Timeout = 60
+	update := tgbotapi.NewUpdate(0)
+	update.Timeout = 60
 
-		updates, _ := bot.GetUpdates(update)
+	updates := bot.GetUpdatesChan(update)
 
-		for _, u := range updates {
-			if u.Message == nil {
-				continue
-			}
+	for u := range updates {
+		if u.Message == nil {
+			continue
+		}
 
-			if reflect.TypeOf(u.Message.Text).Kind() == reflect.String && u.Message.Text != "" {
-				switch u.Message.Text {
-				case "/start":
-					user := &User{
-						TelegramUserID: u.Message.Chat.ID,
-					}
-					_, err = service.CreateUser(user)
-					if err != nil {
-						msg := tgbotapi.NewMessage(u.Message.Chat.ID, "Ошибка в базе.Попробуйте снова.")
-						bot.Send(msg)
-					} else {
-						msg := tgbotapi.NewMessage(u.Message.Chat.ID, startMsg)
-						bot.Send(msg)
-					}
-				case "/list":
-					user := &User{
-						TelegramUserID: u.Message.Chat.ID,
-					}
-					user, err = service.CreateUser(user)
-					if err != nil {
-						msg := tgbotapi.NewMessage(u.Message.Chat.ID, "Ошибка в базе.Попробуйте снова.")
-						bot.Send(msg)
-					}
-					list, err := service.ListUserMangas(*user)
-					if err != nil {
-						msg := tgbotapi.NewMessage(u.Message.Chat.ID, "Ошибка в базе.Попробуйте снова.")
-						bot.Send(msg)
-					}
-					str := ""
-					for i, m := range list {
-						str += m.Name + " - " + strconv.Itoa(i) + "\n"
-					}
-					msg := tgbotapi.NewMessage(u.Message.Chat.ID, str)
+		if reflect.TypeOf(u.Message.Text).Kind() == reflect.String && u.Message.Text != "" {
+			message := strings.Fields(u.Message.Text)
+			switch message[0] {
+			case "/start":
+				user := &User{
+					TelegramUserID: u.Message.Chat.ID,
+				}
+				_, err = service.CreateUser(user)
+				if err != nil {
+					msg := tgbotapi.NewMessage(u.Message.Chat.ID, "Ошибка в базе.Попробуйте снова.")
 					bot.Send(msg)
-				case "/remove":
+				} else {
+					msg := tgbotapi.NewMessage(u.Message.Chat.ID, startMsg)
+					bot.Send(msg)
+				}
+			case "/list":
+				user := &User{
+					TelegramUserID: u.Message.Chat.ID,
+				}
+				user, err = service.CreateUser(user)
+				if err != nil {
+					msg := tgbotapi.NewMessage(u.Message.Chat.ID, "Ошибка в базе.Попробуйте снова.")
+					bot.Send(msg)
+				}
+				list, err := service.ListUserMangas(*user)
+				if err != nil {
+					msg := tgbotapi.NewMessage(u.Message.Chat.ID, "Ошибка в базе.Попробуйте снова.")
+					bot.Send(msg)
+				}
+				str := ""
+				for i, m := range list {
+					str += m.Name + " - " + strconv.Itoa(i) + "\n"
+				}
+				msg := tgbotapi.NewMessage(u.Message.Chat.ID, str)
+				bot.Send(msg)
+			case "/remove":
+				if len(message) > 1 {
 					user := &User{
 						TelegramUserID: u.Message.Chat.ID,
 					}
@@ -81,65 +82,61 @@ func TelegramBotReplier(service *NotifangaService) {
 						msg := tgbotapi.NewMessage(u.Message.Chat.ID, "Ошибка в базе.Попробуйте снова.")
 						bot.Send(msg)
 					}
-					if len(u.Message.Text) > 8 {
-						arg := u.Message.Text[8:]
-						num, err := strconv.Atoi(arg)
-						if err != nil {
-							msg := tgbotapi.NewMessage(u.Message.Chat.ID, "Неправильный ввод.")
-							bot.Send(msg)
-						}
-						if num < len(list) {
-							m := list[num]
-							if err := service.RemoveMangaFromUser(m, user); err != nil {
-								msg := tgbotapi.NewMessage(u.Message.Chat.ID, "Не удалось удалить. Попробуйте снова.")
-								bot.Send(msg)
-							} else {
-								msg := tgbotapi.NewMessage(u.Message.Chat.ID, m.Name+" удален из вашего списка.")
-								bot.Send(msg)
-							}
-						} else {
-							msg := tgbotapi.NewMessage(u.Message.Chat.ID, "Нет такого id в вашем списке.")
-							bot.Send(msg)
-						}
+					arg := message[1]
+					num, err := strconv.Atoi(arg)
+					if err != nil {
+						msg := tgbotapi.NewMessage(u.Message.Chat.ID, "Неправильный ввод.")
+						bot.Send(msg)
 					}
-				default:
-					if strings.Contains(u.Message.Text, "mangalib.me") {
-						link := u.Message.Text
-						user := &User{
-							TelegramUserID: u.Message.Chat.ID,
-						}
-						user, err = service.CreateUser(user)
-						if err != nil {
-							msg := tgbotapi.NewMessage(u.Message.Chat.ID, "Ошибка в базе.Попробуйте снова.")
-							bot.Send(msg)
-						}
-						manga := &Manga{
-							Name:           CrawlName(link),
-							Url:            link,
-							LastChapter:    "",
-							LastChapterUrl: "",
-						}
-						manga, err = service.CreateManga(manga)
-						if err != nil {
-							msg := tgbotapi.NewMessage(u.Message.Chat.ID, "Манга не была найдена.")
-							bot.Send(msg)
-						}
-						if err := service.AddMangaToUser(manga, user); err != nil {
-							msg := tgbotapi.NewMessage(u.Message.Chat.ID, "Манга не была найдена.")
+					if num < len(list) {
+						m := list[num]
+						if err := service.RemoveMangaFromUser(m, user); err != nil {
+							msg := tgbotapi.NewMessage(u.Message.Chat.ID, "Не удалось удалить. Попробуйте снова.")
 							bot.Send(msg)
 						} else {
-							msg := tgbotapi.NewMessage(u.Message.Chat.ID, "Манга добавлена в ваш список.")
+							msg := tgbotapi.NewMessage(u.Message.Chat.ID, m.Name+" удален из вашего списка.")
 							bot.Send(msg)
 						}
 					} else {
-						msg := tgbotapi.NewMessage(u.Message.Chat.ID, "Неправильная ссылка.")
+						msg := tgbotapi.NewMessage(u.Message.Chat.ID, "Нет такого id в вашем списке.")
 						bot.Send(msg)
 					}
 				}
+			default:
+				if strings.Contains(u.Message.Text, "mangalib.me") {
+					link := u.Message.Text
+					user := &User{
+						TelegramUserID: u.Message.Chat.ID,
+					}
+					user, err = service.CreateUser(user)
+					if err != nil {
+						msg := tgbotapi.NewMessage(u.Message.Chat.ID, "Ошибка в базе.Попробуйте снова.")
+						bot.Send(msg)
+					}
+					manga := &Manga{
+						Name:           CrawlName(link),
+						Url:            link,
+						LastChapter:    "",
+						LastChapterUrl: "",
+					}
+					manga, err = service.CreateManga(manga)
+					if err != nil {
+						msg := tgbotapi.NewMessage(u.Message.Chat.ID, "Манга не была найдена.")
+						bot.Send(msg)
+					}
+					if err := service.AddMangaToUser(manga, user); err != nil {
+						msg := tgbotapi.NewMessage(u.Message.Chat.ID, "Манга не была найдена.")
+						bot.Send(msg)
+					} else {
+						msg := tgbotapi.NewMessage(u.Message.Chat.ID, "Манга добавлена в ваш список.")
+						bot.Send(msg)
+					}
+				} else {
+					msg := tgbotapi.NewMessage(u.Message.Chat.ID, "Неправильная ссылка.")
+					bot.Send(msg)
+				}
 			}
 		}
-		TelegramBotCrawler(service)
-		time.Sleep(time.Minute * 5)
 	}
 }
 
@@ -149,16 +146,19 @@ func TelegramBotCrawler(service *NotifangaService) {
 		panic(err)
 	}
 
-	marr, _ := service.GetAllMangas()
+	for {
+		marr, _ := service.GetAllMangas()
 
-	for _, m := range marr {
-		uarr, m := Crawl(*m, service)
-		for _, u := range uarr {
-			msg := tgbotapi.NewMessage(
-				u.TelegramUserID,
-				"Вышла новая "+m.LastChapter+" глава манги "+m.Name+"!\nЧитать тут - "+m.LastChapterUrl,
-			)
-			bot.Send(msg)
+		for _, m := range marr {
+			uarr, m := Crawl(*m, service)
+			for _, u := range uarr {
+				msg := tgbotapi.NewMessage(
+					u.TelegramUserID,
+					"Вышла новая "+m.LastChapter+" глава манги "+m.Name+"!\nЧитать тут - "+m.LastChapterUrl,
+				)
+				bot.Send(msg)
+			}
 		}
+		time.Sleep(time.Minute * 5)
 	}
 }
--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"database/sql"
-	"fmt"
 	"log"
 	"os"
 
@@ -24,114 +23,7 @@ func main() {
 	}
 	service := NewNotifangaService(repo)
 
-	// user1
-	u := &User{
-		TelegramUserID: "telegram1",
-	}
-	u, err = service.CreateUser(u)
-	if err != nil {
-		log.Println("cant create a user", err)
-	}
-	log.Println("user was created", u)
+	go TelegramBotReplier(service)
+	TelegramBotCrawler(service)
 
-	m := &Manga{
-		Name:           CrawlName("https://mangalib.me/one-piece?section=info"),
-		Url:            "https://mangalib.me/one-piece?section=info",
-		LastChapter:    "",
-		LastChapterUrl: "",
-	}
-	log.Println("manga name", m.Name)
-	m, err = service.CreateManga(m)
-	if err != nil {
-		log.Println("cant create manga", err)
-	}
-	log.Println("manga was created", m)
-
-	if err := service.AddMangaToUser(m, u); err != nil {
-		log.Println("cant add manga to user", err)
-	}
-
-	m = &Manga{
-		Name:           CrawlName("https://mangalib.me/onepunchman?section=info"),
-		Url:            "https://mangalib.me/onepunchman?section=info",
-		LastChapter:    "",
-		LastChapterUrl: "",
-	}
-	log.Println("manga name", m.Name)
-	m, err = service.CreateManga(m)
-	if err != nil {
-		log.Println("cant create manga", err)
-	}
-	log.Println("manga was created", m)
-
-	if err := service.AddMangaToUser(m, u); err != nil {
-		log.Println("cant add manga to user", err)
-	}
-	// if err := service.RemoveMangaFromUser(m, u); err != nil {
-	// }
-
-	// // user2
-	// u = &User{
-	// 	TelegramUserID: "telegram2",
-	// }
-	// u, err = service.CreateUser(u)
-	// if err != nil {
-	// 	log.Println("cant create a user", err)
-	// }
-	// log.Println("user was created", u)
-
-	// m = &Manga{
-	// 	Name:           CrawlName("https://mangalib.me/toukyou-revengers?section=info"),
-	// 	Url:            "https://mangalib.me/toukyou-revengers?section=info",
-	// 	LastChapter:    "",
-	// 	LastChapterUrl: "",
-	// }
-	// log.Println("manga name", m.Name)
-	// m, err = service.CreateManga(m)
-	// if err != nil {
-	// 	log.Println("cant create manga", err)
-	// }
-	// log.Println("manga was created", m)
-
-	// if err := service.AddMangaToUser(m, u); err != nil {
-	// 	log.Println("cant add manga to user", err)
-	// }
-
-	// for {
-	marr, err := service.GetAllMangas()
-	for _, manga := range marr {
-		log.Println(manga)
-	}
-	if err != nil {
-		log.Println("cannot get all mangas", err)
-	}
-
-	for _, m := range marr {
-		log.Println("loop started")
-		log.Println("manga list", m)
-		uarr := Crawl(*m, service)
-		for _, user := range uarr {
-			log.Println("user list", user)
-		}
-		for _, u := range uarr {
-			fmt.Println("new chapter!", u.TelegramUserID)
-		}
-	}
-	// time.Sleep(time.Minute * 1)
-	// }
-
-	users, err := service.GetUsers()
-	if err != nil {
-		log.Println("cant get list of all users", err)
-	}
-	log.Println("users:")
-	for _, u := range users {
-		log.Println(u)
-		mangas, err := service.ListUserMangas(*u)
-		if err != nil {
-		}
-		for _, m := range mangas {
-			log.Println(m)
-		}
-	}
 }
